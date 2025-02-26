423146bcde7d0fc9c3073896c84365af4f96be09
YuliaKrasnopol <ykrasnopol@gmail.com>
2025-02-26 20:40:05 +0200

    created Exceptions

--- src/main/java/experiments/Exceptions.java
+++ src/main/java/experiments/Exceptions.java
@@ -1,4 +1,37 @@
 package experiments;
 
 public class Exceptions {
+    public static void main(String[] args) {
+        String[] strArray={"str1","str2","str3"};
+        System.out.println("------");
+        pause();
+        System.out.println("yes");
+        uncheckedException(strArray);
+    }
+
+    private static void uncheckedException(String[] strArray){
+        try{for (int i = 0; i < 4; i++) {
+            System.out.println(strArray[i]);
+
+        }
+        } catch(Exception e){
+            e.getStackTrace();
+            System.out.println("Created Exception");
+            throw new RuntimeException(e);
+        }
+        System.out.println("program works");
+    }
+    private static void pause(){
+        try {
+            checkedException();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void checkedException() throws InterruptedException {
+        Thread.sleep(3000);
+
+    }
+
 }
